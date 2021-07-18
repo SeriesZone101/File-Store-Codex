615dc3b0cc585e4989b74c495d204589fb96cb02
Deepak V <85978656+SeriesZone101@users.noreply.github.com>
2021-07-18 10:47:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -45,7 +45,7 @@ class Bot(Client):
             sys.exit()
 
         self.set_parse_mode("html")
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by 𝘾𝙤𝙙𝙚 𝕏 𝘽𝙤𝙩𝙯\nhttps://t.me/CodeXBotz")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by Joey\nhttps://t.me/DeepakV111")
         self.username = usr_bot_me.username
 
     async def stop(self, *args):
